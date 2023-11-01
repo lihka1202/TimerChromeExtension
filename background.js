eac0773848e107f49ed851addde3771d8bec883f
lihka1202 <88475310+lihka1202@users.noreply.github.com>
2023-11-02 00:29:20 +0800

    chore : timer added and synced with the popup and badge

--- background.js
+++ background.js
@@ -1,6 +1,19 @@
-let time = 0
+chrome.alarms.create({
+    periodInMinutes: 1/60 // this is in minutes
+})
 
-setInterval(()=>{
-    time+=1
-    console.log(time)
-}, 1000)
+chrome.alarms.onAlarm.addListener((alarms) => {
+    chrome.storage.local.get(["timer"], (res) => {
+        const time = res.timer ?? 0
+        // mod the value in the timer every single second
+        chrome.storage.local.set({
+            timer : time + 1
+        })
+
+        // Ensure that this is reflected on the Badge too
+        chrome.action.setBadgeText(
+            {
+                text : `${time + 1}`
+            })
+    })
+})
--- popup.js
+++ popup.js
@@ -1,21 +1,34 @@
 const timeElement = document.getElementById("time")
-const currentTime = new Date().toLocaleTimeString()
+
 
 const nameElement = document.getElementById("name")
-timeElement.textContent = `The time is ${currentTime}`
+const timerCurrent = document.getElementById("time-left")
 
-chrome.action.setBadgeText(
-    {
-        text : "TIMER"
-    },
-    () => {
-        console.log("Finished Setting the Badge Text")
-    }
-)
+
+
+function updateTimeElements() {
+    chrome.storage.local.get(
+        ["timer"],
+        (res) => {
+            timerCurrent.textContent = res.timer ?? 0
+        }
+    )
+
+    const currentTime = new Date().toLocaleTimeString()
+    timeElement.textContent = `The time is ${currentTime}`
+}
+
+updateTimeElements() // This needs to be called due to a one second delay
+setInterval(updateTimeElements, 1000) // 1000 is the timeout
+
 
 // get the data from sync storage and update the name that is displayed
+
+
+
+
 chrome.storage.sync.get(
-    ["name"],
+    ["name", "timer"],
     (res) => {
         const name = res.name ?? "????" // checks if valid, if not get ???
         nameElement.textContent = `Your name is: ${name}`
